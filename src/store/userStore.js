d9f02c606d91380ab8578add5a7e46455ae8a4a2
Elton Lin <eltonlin1998@gmail.com>
2025-03-18 16:57:43 +0900

    Comment

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,3 +1,4 @@
+// This module was created to avoid circular dependencies between store modules, though there could be a better way
 import { writable } from 'svelte/store'
 
 export const user = writable({}) // {} means not logged in, cannot be null
