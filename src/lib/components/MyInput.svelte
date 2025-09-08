f6bcd3effcb031e11cc49e7e142dde845b33cd83
Elton Lin <eltonlin1998@gmail.com>
2025-09-08 19:13:38 +0900

    Try prevenDefaultt + stopPropagation

--- src/lib/components/MyInput.svelte
+++ src/lib/components/MyInput.svelte
@@ -1,24 +1,30 @@
 <script>
-  // TO-DO: auto-focus (or at least, ability to type text without double clicking)
+  // TO-DO: 
+  //   - auto-focus (or at least, ability to type text without double clicking)
+  //   - aligned properly
   import { onMount } from 'svelte'
 
   let { value, onenter, oninput, placeholder } = $props()
   let inputElem = $state(null)
 
   onMount(() => {
-    inputElem.focus()
+    inputElem.focus() // otherwise it's annoying to click twice just to enter text, must work across browsers
   })
 
   function onkeyup (e) {
+    e.preventDefault()
+    e.stopPropagation()
     if (e.key === 'Enter') {
       onenter(e)
     }
   }
 </script>
 
-<input bind:this={inputElem}
-  {placeholder}
-  {value}
-  {oninput}
-  {onkeyup}
->
+<div style="outline: 2px solid red">
+  <input bind:this={inputElem}
+    {placeholder}
+    {value}
+    {oninput}
+    {onkeyup}
+  >
+</div>
