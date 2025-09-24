ac6f43249d44b25c20d6ec69f4728316c13644ce
Elton Lin <eltonlin1998@gmail.com>
2025-09-24 18:30:19 +0900

    Add VITE prefix

--- src/routes/[user]/components/AI/GPT.js
+++ src/routes/[user]/components/AI/GPT.js
@@ -1,7 +1,7 @@
 import { DateTime } from "luxon"
 import OpenAI from "openai"
 const client = new OpenAI({
-  apiKey: import.meta.env.OPENAI_API_KEY,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
 })
 
