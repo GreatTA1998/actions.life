10314b3877b7b08123908b533396fbbad22a34eb
Elton Lin <eltonlin1998@gmail.com>
2025-11-02 19:18:24 +0900

    Fix flexbox area not expanding as expected

--- src/routes/[user]/components/ListsArea/ListsArea.svelte
+++ src/routes/[user]/components/ListsArea/ListsArea.svelte
@@ -5,25 +5,17 @@
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
-    height: calc(100vh - var(--height-navbar));
     align-items: start; /* default is stretch */
     align-content: start; /* prevents column gaps from widening when the area gets larger */
     column-gap: 24px; /* matches dropzone's height */
     padding: 0 24px; /* add same spacing on left/right sides to match column gap */
+    height: 100%;
+    min-width: max-content;
   `
 </script>
 
-<div class="todo-list-container"> 
-  <TodoList 
-    listWidth="320px"
-    willShowCheckbox={true} 
-    cssStyle={wrappingColumnLayout}
-  />
-</div>
-
-<style>
-  .todo-list-container {
-    height: 100%;
-    /* width: 100%; would cause the strange shifting out of screen bug*/
-  }
-</style>
+<TodoList 
+  listWidth="320px"
+  willShowCheckbox={true} 
+  cssStyle={wrappingColumnLayout}
+/>
