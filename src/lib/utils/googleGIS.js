c11e677be6fba9a85a02bb41d6a5f36c922aa825
Elton Lin <eltonlin1998@gmail.com>
2025-12-23 15:49:18 +0900

    .

--- src/lib/utils/googleGIS.js
+++ src/lib/utils/googleGIS.js
@@ -50,7 +50,6 @@ export function loadGoogleIdentityServices () {
     const script = document.createElement('script')
     script.src = 'https://accounts.google.com/gsi/client'
     script.async = true
-    script.defer = true
     script.onload = resolve
     script.onerror = () => reject(new Error('Failed to load Google Identity Services script.'))
     document.head.appendChild(script)
