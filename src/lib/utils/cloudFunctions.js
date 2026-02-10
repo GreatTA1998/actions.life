a2657d3548bbb3f749052a69ece50370e1a01ded
Elton Lin <eltonlin1998@gmail.com>
2026-02-10 10:56:52 +0900

    .

--- src/lib/utils/cloudFunctions.js
+++ src/lib/utils/cloudFunctions.js
@@ -2,7 +2,7 @@ import { getFunctions, httpsCallable, connectFunctionsEmulator } from 'firebase/
 import { app } from '$lib/db/init.js'
 
 const functions = getFunctions(app)
-connectFunctionsEmulator(functions, "127.0.0.1", 5001)
+// connectFunctionsEmulator(functions, "127.0.0.1", 5001)
 
 export function cloudFunction (fnName, params = {}) {
   const fn = httpsCallable(functions, fnName)
