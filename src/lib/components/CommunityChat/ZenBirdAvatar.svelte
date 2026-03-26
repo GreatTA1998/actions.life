f59254507ad73b1a896504b82545e071728f8691
Elton Lin <eltonlin1998@gmail.com>
2026-03-26 18:32:02 +0900

    Disable popover

--- src/lib/components/CommunityChat/ZenBirdAvatar.svelte
+++ src/lib/components/CommunityChat/ZenBirdAvatar.svelte
@@ -15,8 +15,9 @@
 
 <PopoverMenu>
   {#snippet activator({ id, anchorName })}
+    <!-- `popovertarget` set to nothing, because the popover is only for previewing new color filters -->
     <button
-      popovertarget={size !== 'sm' ? id : ''}
+      popovertarget=""
       style:anchor-name={anchorName}
       class="flex items-center justify-center overflow-hidden rounded-full bg-neutral-100 shadow-sm ring-1 ring-black/5 {sizeClass} {className}"
     >
