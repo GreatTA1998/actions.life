df85370ffa38bde4664acf1baf03f2c0dd4386c5
Elton Lin <eltonlin1998@gmail.com>
2026-04-16 17:01:11 +0900

    .

--- src/routes/(home)/components/PhoneDisplay.svelte
+++ src/routes/(home)/components/PhoneDisplay.svelte
@@ -5,9 +5,7 @@
 </script>
 
 <div class="w-[min(88vw,320px)] mx-auto drop-shadow-[0_20px_50px_rgba(0,0,0,0.5)]">
-  <div class="{iphone_16_aspect_ratio} bg-black rounded-[40px] p-[6px] shadow-2xl">
-    <div class={['h-full bg-white', 'rounded-[34px] overflow-hidden']}>
-      {@render children()}
-    </div>
+  <div class="{iphone_16_aspect_ratio} rounded-[34px] overflow-hidden outline outline-[6px] outline-black shadow-2xl">
+    {@render children()}
   </div>
 </div>
