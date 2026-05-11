a2d45d9c6a03e2d07b08e1d7369d4aca5fde19eb
Elton Lin <eltonlin1998@gmail.com>
2026-05-11 15:40:53 +0900

    Bug bot fixes

--- src/lib/utils/errors.js
+++ src/lib/utils/errors.js
@@ -2,13 +2,14 @@ import { dev } from '$app/environment'
 import { randomID } from '$lib/utils/core.js'
 import { user, loading } from '$lib/store'
 import { get } from 'svelte/store'
+import { setFirestoreDoc } from '$lib/db/helpers.js'
 
 let logs = 0
 const limit = 10
 
 export async function reportError ({ subject, content }) {
   loading.set(false)
-  alert('Unexpected error:', subject, content)
+  alert(`UNEXPECTED ERROR ${subject}: ${content}`)
   if (!dev && logs < limit) {
     setFirestoreDoc(`/errors/${randomID()}`, {
       subject, content,
