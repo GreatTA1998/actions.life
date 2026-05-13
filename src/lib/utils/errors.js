2bdfb4e3f8816d81e009b47e8132aa30d2c40aa9
Elton Lin <eltonlin1998@gmail.com>
2026-05-13 15:13:03 +0900

    more detailed error logs

--- src/lib/utils/errors.js
+++ src/lib/utils/errors.js
@@ -3,6 +3,7 @@ import { randomID } from '$lib/utils/core.js'
 import { user, loading } from '$lib/store'
 import { get } from 'svelte/store'
 import { setFirestoreDoc } from '$lib/db/helpers.js'
+import { DateTime } from 'luxon'
 
 let logs = 0
 const limit = 10
@@ -15,7 +16,10 @@ export async function reportError ({ subject, content }) {
     setFirestoreDoc(`/errors/${randomID()}`, {
       subject, content,
       uid: get(user).uid || '',
-      email: get(user).email || '' 
+      email: get(user).email || '',
+      utc: DateTime.now().toFormat('yyyy-MM-dd'),
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      userAgent: navigator.userAgent
     })
     logs += 1
   }
