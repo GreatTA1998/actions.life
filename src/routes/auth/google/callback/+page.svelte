9661edbbf3fe96bbc1f7f323d500db4bdddf8696
Elton Lin <eltonlin1998@gmail.com>
2026-05-25 15:56:39 +0900

    Fix wrong parameter

--- src/routes/auth/google/callback/+page.svelte
+++ src/routes/auth/google/callback/+page.svelte
@@ -6,6 +6,7 @@
     GoogleAuthProvider,
     AuthErrorCodes
   } from 'firebase/auth'
+  import User from '$lib/db/models/User.js'
   import { cloudFunction } from '$lib/utils/cloudFunctions.js'
   import { firebaseAuth } from '$lib/store'
   import { get } from 'svelte/store'
@@ -22,7 +23,7 @@
     const credential = GoogleAuthProvider.credential(data.idToken, data.accessToken)
 
     try {
-      const result = await linkWithCredential(credential)
+      const result = await linkWithCredential(get(firebaseAuth).currentUser, credential)
       console.log("result =", result)
       await User.update({ email: result.user.email })
       goto(`/${result.user.uid}`) // `onAuthStateChanged` doesn't trigger with account linking
