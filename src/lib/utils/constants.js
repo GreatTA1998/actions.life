d55df69f139917630f47db1537ae92e4ee8b99ac
Elton Lin <eltonlin1998@gmail.com>
2026-08-21 16:51:56 +0900

    Fix mobile home page looking horrible

--- src/lib/utils/constants.js
+++ src/lib/utils/constants.js
@@ -26,6 +26,12 @@ export const WIDTHS = Object.freeze({
   PANEL_MAX: breakpoints.desktop / goldenRatio
 })
 
+export const calendarDensity = Object.freeze({
+  wide: Object.freeze({ pixelsPerHour: 80, calColumnWidth: 260 }),
+  dense: Object.freeze({ pixelsPerHour: 50, calColumnWidth: 160 }),
+  mini: Object.freeze({ pixelsPerHour: 31, calColumnWidth: 100 }),
+})
+
 export const MOBILE_SAFE_INSET = 48
 
 export const TOUCH = Object.freeze({
