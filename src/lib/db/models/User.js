5e8cf80f4ed5001c6e3003cfbb7519304e96450d
Elton Lin <eltonlin1998@gmail.com>
2026-08-21 16:27:35 +0900

    Change default

--- src/lib/db/models/User.js
+++ src/lib/db/models/User.js
@@ -29,8 +29,8 @@ const User = {
     avatarFilter: z.string().default(''),
 
     tags: z.record(z.object({ color: z.string(), name: z.string()})).default({}),
-    pixelsPerHour: z.number().default(50),
-    calColumnWidth: z.number().default(160),
+    pixelsPerHour: z.number().default(80),
+    calColumnWidth: z.number().default(260),
 
     selectedGoogleCalendarIds: z.array(z.string()).optional() // to deprecate
   }),
